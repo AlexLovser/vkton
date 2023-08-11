aec1da0941ea31b830361d1016049b59e5bb40e1
Alex Lovser <tabalex2005@gmail.com>
2023-08-11 15:43:42 +0300

    фвы

--- main.py
+++ main.py
@@ -29,24 +29,24 @@ bot = Bot(TOKEN)
 
 
 
-# ## VK-ton 
-# Это модуль для упрощенного создания VK ботов на Python
+## VK-ton 
+Это модуль для упрощенного создания VK ботов на Python
 
-# ### Installing dependencies
-# `pip install vk-ton`
+### Installing dependencies
+`pip install vk-ton`
 
 
-# ## Getting started
-# ```py
-# from vkton import Bot, Commands, Context
+## Getting started
+```py
+from vkton import Bot, Commands, Context
 
-# @Commands.command(keywords=["Поздароваться", 'Say hello'])
-# def hello(ctx: Context):
-#     ctx.user.send(
-#         'Hello, my Friend! You are using VK-ton by Alex Lovser! Nice to see you!'
-#     )
+@Commands.command(keywords=["Поздароваться", 'Say hello'])
+def hello(ctx: Context):
+    ctx.user.send(
+        'Hello, my Friend! You are using VK-ton by Alex Lovser! Nice to see you!'
+    )
 
-# ```
+```
 
 
 bot.run()
